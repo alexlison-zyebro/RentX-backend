ef6710cdd0e569590acf12f76d6746dcbaf3e085
alexlison <alexlison910@gmail.com>
2026-02-11 19:20:45 +0530

    all products routes fixed

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -3,10 +3,12 @@ import { authenticate } from "../middlewares/auth.middleware.js";
 import { viewAllCategories } from '../controllers/category.controllers.js';
 import { createRentRequest, getBuyerRentRequests, getRentRequestById } from "../controllers/rentRequest.controllers.js";
 import { updateBuyer, viewBuyerById } from "../controllers/buyer.controllers.js";
+import { getAllProducts } from "../controllers/product.controllers.js";
 
 const router = express.Router();
 
 router.use(authenticate);
+router.post("/allProducts", getAllProducts); 
 router.post('/categories/all', viewAllCategories);
 router.post("/createRequest", createRentRequest);
 router.post("/my-requests", getBuyerRentRequests);
